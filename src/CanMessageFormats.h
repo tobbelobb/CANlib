5297b0eef37f0469f3932047fade50e0395b5a33
Torbjørn Ludvigsen <tobben@fastmail.fm>
2025-10-20 14:40:27 +0200

    Temporary allow larger packets on host

--- src/CanMessageFormats.h
+++ src/CanMessageFormats.h
@@ -1355,6 +1355,10 @@ union CanMessage
 	CanMessageEnableStallEndstop enableStallEndstop;
 };
 
+#if RRF_HOST_BUILD
+static_assert(sizeof(CanMessage) <= 128, "CAN message too big for host shim");	// host pointers widen some structs; revisit when host layout stabilises
+#else
 static_assert(sizeof(CanMessage) <= 64, "CAN message too big");		// check none of the messages is too large
+#endif
 
 #endif /* SRC_CAN_CANMESSAGEFORMATS_H_ */
